2cd1c7fe86ab24b0ba6e19dc5b60f7671089c377
dagrv <rust.91@icloud.com>
2024-07-18 11:10:18 +0200

    Convex first use

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
-import Image from "next/image";
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
+import { createDocument } from '../convex/documents';
+
 
 export default function Home() {
+	const documents = useQuery(api.documents.getDocuments);
+	const createDocument = useMutation(api.documents.createDocument)
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<Unauthenticated>
-				<div className="bg-emerald-600 p-2 rounded-md">
+				<div className="bg-sky-600 p-2 rounded-md">
 					<SignInButton />
 				</div>
 				
 			</Unauthenticated>
 			<Authenticated>
 				<UserButton />
+
+				<button onClick={() => {
+					createDocument({title: 'hello'})
+				}} className="bg-sky-500 p-2 rounded-lg w-full">Click here</button>
+
+				{documents?.map((doc) => (
+					<div key={doc._id}>{doc.title}</div>
+				))}
 			</Authenticated>
 		</main>
 	);
